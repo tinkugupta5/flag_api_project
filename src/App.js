dce2e5aa037148d4808ab31be045fd8efaa47948
tinku gupta <tinku.gupta4@gmail.com>
2022-08-03 23:50:16 +0530

    just add a div in a file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Flag api project</h1>
+     <div className="container">
+      <h1>This is an example</h1>
+     </div>
     </div>
   );
 }
